7a86ae1908f541cbb7b086d4bd5142e9f1fccda5
1lmelo <lucas_santosmelo@hotmail.com>
2023-04-24 01:48:12 -0300

    Layoult system of login

---
+++ LoginSystem/LoginSystem/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public DashboardController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Recovery()
+        {
+            return View();
+        }
+    }
+}
